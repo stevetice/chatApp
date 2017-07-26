453a4f57656a874ef32176352a8ca35c1753793d
stevetice <tice.steve@gmail.com>
2017-07-26 20:50:24 +0000

    Add componentDidMount to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,18 @@ class App extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({loading: true})  // change the state. this calls render() and the component updates.
-  //   }, 3000)
-  // }
-
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
 
   render() {
     console.log("Rendering <App/>");
